db0e47416b92fc7305291f6a8c78ae28d04c3c2c
Nick Gartmann <nick.gartmann@gmail.com>
2017-08-03 15:34:33 -0500

    Added search

--- location.js
+++ location.js
@@ -1,3 +1,4 @@
+var ElasticLunr = require("elasticlunr");
 var config = require('./config');
 
 const LOCATIONS_TAB_IDENTIFIER="Locations";
@@ -5,6 +6,19 @@ const LOCATIONS_TAB_IDENTIFIER="Locations";
 // Module locals
 var client = config.client()(LOCATIONS_TAB_IDENTIFIER);
 var cachePromise = null;
+
+const boosts = {
+  "Name": {boost: 1},
+  "Address": {boost: 0.5},
+  "Service Names": {boost: 1},
+  "Service Descriptions": {boost: 0.5},
+  "Service Categories": {boost: 1},
+  "Search": {boost: 3},
+}
+
+var elasticIndex = ElasticLunr(function() {
+  Object.keys(boosts).forEach(this.addField.bind(this))
+});
 
 module.exports = {
   get: function(id) {
@@ -19,12 +33,18 @@ module.exports = {
     if(cachePromise === null) this.refresh();
     return cachePromise;
   },
-  search: function() {
+  search: function(query) {
     if(cachePromise === null) this.refresh();
     return cachePromise.then(function(locations) {
-      // TODO: execute the search
-      return locations;
-    })
+      return Promise.all(elasticIndex.search(query, {fields: boosts}).map(function(result) {
+        return this.get(result.ref).then(function(location) {
+          return {
+            location: location,
+            relevance: result.score
+          }
+        })
+      }.bind(this)))
+    }.bind(this))
   },
   refresh: function() {
     cachePromise = new Promise(function(resolve, reject) {
@@ -36,6 +56,29 @@ module.exports = {
     return cachePromise;
   },
   index: function(records) {
+    records.map(this._createDocument.bind(this))
+    .forEach(function(doc) {
+      elasticIndex.addDoc(doc);
+    })
     return records;    
-  }
+  },
+  _createDocument: function(record) {
+    return {
+      "id": record.id,
+      "Name": record.get("Name"),
+      "Address": record.get("Address"),
+      "Service Names": joinFields(record, ["Service 1 Name", "Service 2 Name", "Service 3 Name"]),
+      "Service Descriptions": joinFields(record, ["Service 1 Description", "Service 2 Description", "Service 3 Description"]),
+      "Service Categories": joinFields(record, ["Service 1 Categories", "Service 2 Categories", "Service 3 Categories"]),
+      "Search": record.get("Search")
+    }
+  },
+}
+
+function joinFields(record, fields) {
+  return fields.map(function(field) {
+    return record.get(field)
+  }).reduce(function(acc, el) {
+    return acc.concat(el)
+  }, []).join(" ");
 }
--- organization.js
+++ organization.js
@@ -1,3 +1,4 @@
+var ElasticLunr = require("elasticlunr");
 var config = require('./config');
 
 const ORGANIZATIONS_TAB_IDENTIFIER="Organizations";
@@ -5,6 +6,20 @@ const ORGANIZATIONS_TAB_IDENTIFIER="Organizations";
 // Module locals
 var client = config.client()(ORGANIZATIONS_TAB_IDENTIFIER);
 var cachePromise = null;
+
+const boosts = {
+  "Name": {boost: 1},
+  "Address": {boost: 0.25},
+  "Mission Statement": {boost: 1},
+  "Description": {boost: 1},
+  "Nicknames": {boost: 3},
+  "Search": {boost: 3},
+  "Categores": {boost: 1}
+}
+
+var elasticIndex = ElasticLunr(function() {
+  Object.keys(boosts).forEach(this.addField.bind(this))
+});
 
 module.exports = {
   get: function(id) {
@@ -19,12 +34,18 @@ module.exports = {
     if(cachePromise === null) this.refresh();
     return cachePromise;
   },
-  search: function() {
+  search: function(query) {
     if(cachePromise === null) this.refresh();
     return cachePromise.then(function(organizations) {
-      // TODO: execute the search
-      return organizations;
-    })
+      return Promise.all(elasticIndex.search(query, {fields: boosts}).map(function(result) {
+        return this.get(result.ref).then(function(organization) {
+          return {
+            organization: organization,
+            relevance: result.score
+          }
+        })
+      }.bind(this)))
+    }.bind(this))
   },
   refresh: function() {
     cachePromise = new Promise(function(resolve, reject) {
@@ -36,6 +57,22 @@ module.exports = {
     return cachePromise;
   },
   index: function(records) {
-    
-  }
+    records.map(this._createDocument.bind(this))
+    .forEach(function(doc) {
+      elasticIndex.addDoc(doc);
+    })
+    return records;    
+  },
+  _createDocument: function(record) {
+    return {
+      "id": record.id,
+      "Name": record.get("Name"),
+      "Address": record.get("Address"),
+      "Mission Statement": record.get("Mission Statement"), 
+      "Description": record.get("Description"),
+      "Nicknames": record.get("Organization nicknames"), 
+      "Categories": (record.get("Categories") || []).join(" "),
+      "Search": record.get("Search")
+    }
+  },
 }
